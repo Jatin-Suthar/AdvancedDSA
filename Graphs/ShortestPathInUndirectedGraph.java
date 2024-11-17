aa315809594cb4c1a5c0138ad4a7966a4c93a15c
Jatin-Suthar <jatinsuthar300@gmail.com>
2024-11-17 18:15:45 +0530

    Shortest distance in undirected graph with unit distance

--- Graphs/ShortestPathInUndirectedGraph.java
+++ Graphs/ShortestPathInUndirectedGraph.java
@@ -4,65 +4,45 @@ import java.io.IOException;
 import java.util.*;
 
 public class ShortestPathInUndirectedGraph {
-
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int m=sc.nextInt();
-            int[][] edge = new int[m][2];
-            for(int i=0;i<m;i++){
-                edge[i][0]=sc.nextInt();
-                edge[i][1]=sc.nextInt();
-            }
-            int src=sc.nextInt();
-            Solution_ShortestPathInUndirectedGraph obj = new Solution_ShortestPathInUndirectedGraph();
-            int[] res = obj.shortestPath(edge,n,m,src);
-            for(int i=0;i<n;i++){
-                System.out.print(res[i]+" ");
-            }
-            System.out.println();
-        }
-    }
-
 }
 
 class Solution_ShortestPathInUndirectedGraph {
 
     public int[] shortestPath(int[][] edges,int n,int m ,int src) {
-        int[] distance = new int[n];
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=0; i<n; i++) {
-            list.add(new ArrayList<>());
-            distance[i]=Integer.MAX_VALUE;
-        }
+        Map<Integer, List<Integer>> adjMap = new HashMap<>();
+
         for(int[] edge: edges) {
-            int v = edge[0];
-            int u = edge[1];
-            list.get(v).add(u);
+            List<Integer> list1 = adjMap.getOrDefault(edge[0], new ArrayList<>());
+            list1.add(edge[1]);
+            adjMap.put(edge[0], list1);
+            List<Integer> list2 = adjMap.getOrDefault(edge[1], new ArrayList<>());
+            list2.add(edge[0]);
+            adjMap.put(edge[1], list2);
         }
-        distance[src]=0;
-        boolean[] visited = new boolean[n];
-        dfs(list, src, visited, distance);
-        for(int i=0; i<n; i++) {
-            if(distance[i]==Integer.MAX_VALUE) {
-                distance[i]=-1;
-            }
-        }
-        return distance;
-    }
-    public void dfs(List<List<Integer>> adj, int src, boolean[] visited, int[] distance) {
-        visited[src] = true;
-        for(int idx: adj.get(src)) {
-            if(distance[src]!=Integer.MAX_VALUE) {
-                if(!visited[idx]) {
-                    if(distance[src]+1<distance[idx]) {
-                        distance[idx]=distance[src]+1;
-                    }
-                    dfs(adj, idx, visited, distance);
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            List<Integer> list = adjMap.getOrDefault(cur, new ArrayList<>());
+            for (int next : list) {
+                if (dist[next] > dist[cur] + 1) {
+                    dist[next] = dist[cur] + 1;
+                    queue.add(next);
                 }
             }
         }
+
+        for(int i=0;i<n;i++) {
+            if(dist[i]==Integer.MAX_VALUE) {
+                dist[i]=-1;
+            }
+        }
+
+        return dist;
     }
 }
